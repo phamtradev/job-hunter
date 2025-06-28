7239c329ee0d0a18f47c8c8d4259cd8e5e7db964
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-06-28 22:37:05 +0700

    rm BackEnd

--- BackEnd/src/main/java/vn/phamtra/jobhunter/service/error/IdInvalidException.java
+++
@@ -1,7 +0,0 @@
-package vn.phamtra.jobhunter.service.error;
-
-public class IdInvalidException extends Exception {
-    public IdInvalidException(String message) {
-        super(message);
-    }
-}
