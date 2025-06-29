911b2b6a12473d37b040f1121697b844e2fa8591
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-06-29 10:56:48 +0700

    format respone before sending

--- BackEnd/src/main/java/vn/phamtra/jobhunter/util/FormatRestRespone.java
+++ BackEnd/src/main/java/vn/phamtra/jobhunter/util/FormatRestRespone.java
@@ -1,11 +1,14 @@
 package vn.phamtra.jobhunter.util;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import vn.phamtra.jobhunter.domain.RestRespone;
 
 @ControllerAdvice
 public class FormatRestRespone implements ResponseBodyAdvice {
@@ -17,6 +20,20 @@ public class FormatRestRespone implements ResponseBodyAdvice {
 
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-        return body;
+        HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+        int status = servletResponse.getStatus();
+
+        RestRespone<Object> res = new RestRespone<Object>();
+        res.setStatusCode(status);
+        if (status >= 400) {
+            //case error
+            return body;
+        } else {
+            //case success
+            res.setData(body);
+            res.setMessage("CALL API SUCCESS");
+        }
+
+        return res;
     }
 }
