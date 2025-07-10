fc0f594b300436808202f834642eb86974ced49c
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-07-10 12:12:06 +0700

    create sample data in database

--- BackEnd/src/main/java/vn/phamtra/jobhunter/repository/RoleRepository.java
+++ BackEnd/src/main/java/vn/phamtra/jobhunter/repository/RoleRepository.java
@@ -8,4 +8,6 @@ import vn.phamtra.jobhunter.domain.Role;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long>, JpaSpecificationExecutor<Role> {
     boolean existsByName(String name);
+
+    Role findByName(String name);
 }
