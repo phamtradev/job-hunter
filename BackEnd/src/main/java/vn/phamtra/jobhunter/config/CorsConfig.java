3ac59f19926bbc33518fd674a05fdcde31e67c47
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-12-09 14:53:03 +0700

    feat: add respone link from backend deploy

--- BackEnd/src/main/java/vn/phamtra/jobhunter/config/CorsConfig.java
+++ BackEnd/src/main/java/vn/phamtra/jobhunter/config/CorsConfig.java
@@ -13,16 +13,23 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4173", "http://localhost:4173", "http://localhost:5173")); //cho phép kết nối đến localhost của frontend
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed methods
+        
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "http://localhost:4173",
+                "http://localhost:5173",
+
+
+                "https://job-hunter-pi.vercel.app"
+        ));
+
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "x-no-retry"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
-        // How long the response from a pre-flight request can be cached by clients
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // Apply this configuration to all paths
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
 }
